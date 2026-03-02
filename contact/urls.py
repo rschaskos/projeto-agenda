6856205efadb3ec918b8fc33194de6974b0a3fd3
rschaskos <rschaskos@icloud.com>
2026-03-02 20:25:31 -0300

    feat: add contact detail and search routes

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from contact import views
 
+# esse import busca pelo arquivo 'view.py' no __init__.py da pasta view
+from contact import views 
 app_name = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
 ]
