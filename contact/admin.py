b49e0c061ad903e9761df2df2b958f8d6942ef5a
rschaskos <rschaskos@icloud.com>
2026-03-02 20:25:39 -0300

    refactor: improve django admin for contacts

--- contact/admin.py
+++ contact/admin.py
@@ -3,12 +3,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'show',)
     ordering = ('id',)
     search_fields = ('id', 'first_name', 'last_name',)
-    list_per_page = 10
+    list_per_page = 15
     list_max_show_all = 50
     list_display_links = ('id', 'first_name',) 
+    list_editable = ('show',)
 
 
 @admin.register(models.Category)
